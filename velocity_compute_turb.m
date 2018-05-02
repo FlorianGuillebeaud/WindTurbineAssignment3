4edb330aa77f7a4f00925f43b9e7761ee557645f
FlorianGuillebeaud <36263524+FlorianGuillebeaud@users.noreply.github.com>
2018-05-02 14:12:24 +0200

    Update velocity_compute_turb.m

--- velocity_compute_turb.m
+++ velocity_compute_turb.m
@@ -1,8 +1,8 @@
 %% Compute the relative wind velocity in Coordinate system 4 in y and z direction %%
 
-function [Vrel_y, Vrel_z] = velocity_compute_turb(u, b, k, Wy, Wz, Theta_wing1, Theta_wing2, Theta_wing3, Uy_dot, Uz_dot)
+function [Vrel_y, Vrel_z] = velocity_compute_turb(u_turb, b, k, Wy, Wz, Theta_wing1, Theta_wing2, Theta_wing3, Uy_dot, Uz_dot)
          
-global a_12 a_21 a_34 blade_data H Ls omega0 Theta_cone
+global a_12 a_21 a_34 blade_data H Ls omega0 Theta_cone V_0
         
         rt = [H 0 0] ; 
         rs = a_21*[0 0 -Ls]' ;
@@ -15,7 +15,7 @@ global a_12 a_21 a_34 blade_data H Ls omega0 Theta_cone
             a_14_1 = a_34*a_23_1*a_12 ;
             a_41_1 = a_14_1' ;
             
-            V0_4 = a_14_1*[0 0 V_0+u]' ;
+            V0_4 = a_14_1*[0 0 V_0+u_turb]' ;
             Vrel_y = V0_4(2) + Wy - omega0*blade_data(k)*cos(Theta_cone) - Uy_dot ;
             Vrel_z = V0_4(3) + Wz - Uz_dot;
             
@@ -26,7 +26,7 @@ global a_12 a_21 a_34 blade_data H Ls omega0 Theta_cone
             a_14_2 = a_34*a_23_2*a_12 ;
             a_41_2 = a_14_2' ;            
          
-            V0_4 = a_14_2*[0 0 V_0+u]' ;
+            V0_4 = a_14_2*[0 0 V_0+u_turb]' ;
             Vrel_y = V0_4(2) + Wy - omega0*blade_data(k)*cos(Theta_cone) - Uy_dot ;
             Vrel_z = V0_4(3) + Wz - Uz_dot;
             
@@ -37,7 +37,7 @@ global a_12 a_21 a_34 blade_data H Ls omega0 Theta_cone
             a_14_3 = a_34*a_23_3*a_12 ;
             a_41_3 = a_14_3' ; 
             
-            V0_4 = a_14_3*[0 0 V_0+u]' ;
+            V0_4 = a_14_3*[0 0 V_0+u_turb]' ;
             Vrel_y = V0_4(2) + Wy - omega0*blade_data(k)*cos(Theta_cone)- Uy_dot ;
             Vrel_z = V0_4(3) + Wz - Uz_dot ;
              
